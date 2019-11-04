605c0e650f098effe6ca81fe0ae86fc00dad133b
Milo Calvo-Platero <macp92@gmail.com>
2019-11-04 17:17:50 -0500

    started some front end stuff, not working yet so dont merge

---
+++ client/components/accountPage.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+export class accountPage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Order History</h3>
+        <h3>Account info</h3>
+        <h3>Favorites</h3>
+        <h3>Wishlist</h3>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(accountPage)
