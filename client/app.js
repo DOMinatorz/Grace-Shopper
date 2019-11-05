3c8564805c50e361d18317b238669da946880b52
Gerard Shapiro <gshapiro78@gmail.com>
2019-11-05 12:12:35 -0500

    single

--- client/app.js
+++ client/app.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <AllBracelets />
+      {/* <AllBracelets /> */}
       <Routes />
     </div>
   )
--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,8 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
+import SingleBracelet from './components/single-bracelet'
+import AllBracelets from './components/all-bracelets'
 
 /**
  * COMPONENT
@@ -19,6 +21,8 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route exact path="/bracelets" component={AllBracelets} />
+        <Route path="/bracelets/:id" component={SingleBracelet} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn && (
