2b43da7de01d86a7fea2c61513edad251f981079
Lillian Moriarty <lilybmoriarty@gmail.com>
2019-11-08 15:17:50 -0500

    added isAdmin check to api/users route

--- server/api/users.js
+++ server/api/users.js
@@ -1,18 +1,20 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
+const isAdmin = require('./utils')
+
 module.exports = router
 
 router.get('/', async (req, res, next) => {
+  console.log('this is req.user', req.user)
   try {
-    console.log('do i go in here?')
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'firstName', 'lastName', 'email', 'address']
-    })
-    res.send(users)
-    if (!users) return res.status(404).send('There are no users')
+    if (!isAdmin(req.user)) res.send('GET YO HANDS OFF OUR USERS')
+    else {
+      const users = await User.findAll({
+        attributes: ['id', 'firstName', 'lastName', 'email', 'address']
+      })
+      res.send(users)
+      if (!users) return res.status(404).send('There are no users')
+    }
   } catch (err) {
     next(err)
   }
