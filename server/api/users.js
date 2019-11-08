5ff2033b51063b7ba0a137e1158c128d6c8c3258
Lillian Moriarty <lilybmoriarty@gmail.com>
2019-11-08 15:07:52 -0500

    fixed issue that made the /users route not work

--- server/api/users.js
+++ server/api/users.js
@@ -4,13 +4,15 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
+    console.log('do i go in here?')
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
       attributes: ['id', 'firstName', 'lastName', 'email', 'address']
     })
-    res.json(users)
+    res.send(users)
+    if (!users) return res.status(404).send('There are no users')
   } catch (err) {
     next(err)
   }
